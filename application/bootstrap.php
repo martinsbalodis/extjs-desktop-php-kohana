3be60d1596f7bba241b485c9bfd190efd19d06f4
Mārtiņš Balodis <martins256@gmail.com>
2011-12-30 12:47:17 +0200

    Added: extjs desktop module

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -105,6 +105,7 @@ Kohana::modules(array(
 	// 'orm'        => MODPATH.'orm',        // Object Relationship Mapping
 	// 'unittest'   => MODPATH.'unittest',   // Unit testing
 	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
+	'extjsdesktop'  => MODPATH.'extjsdesktop',  // User guide and API documentation
 	));
 
 /**
